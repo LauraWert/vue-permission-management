3e72e62bdcb718dc2d55b597f3f87b7d954d4c8d
Rick <r.aikema@ultraware.nl>
2018-06-18 10:47:47 +0200

    Remove merge from webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,27 @@
-const merge = require('webpack-merge')
 const path = require('path')
 
-var config =
-  {
-    mode: 'production',
-    output: {
-      path: path.resolve(__dirname, './dist/'),
-    },
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          loader: 'babel-loader',
-          include: [path.resolve(__dirname, './src')],
-        },
-      ],
-    },
-    resolve: {
-      extensions: ['.js'],
-      alias: {
-        src: path.resolve(__dirname, './src'),
+module.exports = {
+  mode: 'production',
+  entry: path.resolve(__dirname, './src/index.js'),
+  output: {
+    path: path.resolve(__dirname, './dist/'),
+    filename: 'index.js',
+    libraryTarget: 'umd',
+    library: 'vue-permission-management',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        include: [path.resolve(__dirname, './src')],
       },
+    ],
+  },
+  resolve: {
+    extensions: ['.js'],
+    alias: {
+      src: path.resolve(__dirname, './src'),
     },
-  }
-
-module.exports = [
-  merge(config, {
-    entry: path.resolve(__dirname, './src/index.js'),
-    output: {
-      filename: 'index.js',
-      libraryTarget: 'umd',
-      library: 'vue-permission-management',
-    },
-  }),
-]
+  },
+}
